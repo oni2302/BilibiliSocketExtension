5770ba7bf3c27719df545097e931961a902ee108
nnp2302 <65790507+nnp2302@users.noreply.github.com>
2023-09-27 01:08:59 +0700

    Revert "1"
    This reverts commit 46963b81da2f332c69710bc794d613e6df249a1b.

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const WebSocket = require('ws');
-var ip = "icy-elastic-earth.glitch.me";
-const wss = new WebSocket.Server({ port:8080 });
-console.log('Socket is listening at: ws://icy-elastic-earth.glitch.me:8080');
+const http = require('http');
+const os = require('os');
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('WebSocket Server');
+});
+var ip = "";
+const wss = new WebSocket.Server({ server });
+
 const connections = {}; // Lưu trữ kết nối bằng ID hoặc tên người dùng
 
-wss.on('connection', (ws) => {
+wss.on('connection', (ws, request) => {
   // Gán một ID hoặc tên người dùng cho kết nối 
   console.log('Client connected');
   let id;
@@ -12,7 +18,7 @@ wss.on('connection', (ws) => {
     var request = JSON.parse(message.toString());
     if (request.action === 'addSocket') {
       connections[request.token] = ws;
-      ws.send('{"action":"addSocket","socket":"' + ip + ':8080","token":"' + request.token + '"}');
+      ws.send('{"action":"addSocket","socket":"' + getLocalIPAddress() + ':8080","token":"' + request.token + '"}');
       console.log(request.token);
     } else if (request.action === "fontSize") {
       var id = request.to;
@@ -28,6 +34,24 @@ wss.on('connection', (ws) => {
 
   ws.on('close', () => {
     console.log('Client disconnected ' + id);
+    // Xóa kết nối đã đóng khỏi danh sách connections
     delete connections[id];
   });
 });
+
+server.listen(8080, () => {
+  console.log('Server is listening on port 8080');
+});
+function getLocalIPAddress() {
+  const networkInterfaces = os.networkInterfaces();
+  // Assuming you want the first non-internal IPv4 address
+  for (const interfaceName in networkInterfaces) {
+    const interface = networkInterfaces[interfaceName];
+    for (const item of interface) {
+      if (item.family === 'IPv4' && !item.internal) {
+        return item.address;
+      }
+    }
+  }
+  return '127.0.0.1'; // Default to localhost if no valid IP found
+}
